dfa352f8b030e5fc4bc8f4c52ca1c1c6a1f2fc12
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-09-29 22:17:25 +0300

    Load main qml file at start

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->qmlview->setSource(QUrl(":/aikarauta/qml/main.qml"));
 }
 
 MainWindow::~MainWindow()
